e844224036e2f6b13be1eca2ed75d831a27f048c
Zub Yakov <yakzub@gmail.com>
2025-10-21 17:48:39 +0500

    empty project added

---
+++ TrainingSessions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainingSessions
+//
+//  Created by Yakov on 21.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
